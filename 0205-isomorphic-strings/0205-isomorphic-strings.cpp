0eeff6c3f10d288d970b2524fdcbf66d465d0556
PiyushMohata <98268606+PiyushMohata@users.noreply.github.com>
2023-08-07 23:37:24 +0530

    Time: 7 ms (68.71%), Space: 7.2 MB (7.56%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -4,10 +4,12 @@ public:
         unordered_map<char,char>m,m1;
         for(int i=0; i<s.size(); i++)
         {
-            if((m[s[i]] && m[s[i]]!=t[i]) || (m1[t[i]] && m1[t[i]]!=s[i]))
+            if(m[s[i]] && m[s[i]]!=t[i])
                 return false;
-            m[s[i]]=t[i];
-            m1[t[i]]=s[i];
+            if(m1[t[i]] && m1[t[i]]!=s[i])
+                return false;
+            m[s[i]] = t[i];
+            m1[t[i]] = s[i];
         }
         return true;
     }
