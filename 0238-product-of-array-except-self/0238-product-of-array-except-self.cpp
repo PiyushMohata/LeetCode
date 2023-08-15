c8132362927178a3776e7c4e04049c162a77cebf
PiyushMohata <98268606+PiyushMohata@users.noreply.github.com>
2023-08-15 22:10:01 +0530

    Time: 23 ms (68.02%), Space: 24.9 MB (37.98%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int>answer(nums.size(),0),prefix,postfix(nums.size(),0);
-        prefix.push_back(nums[0]);
-        postfix[nums.size()-1]=nums[nums.size()-1];
+        int prefix=1,postfix=1;
+        vector<int>ans;
+
         for(int i=1; i<nums.size(); i++)
         {
-            prefix.push_back(nums[i]*prefix[i-1]);
-        } 
-
-        for(int i=nums.size()-2; i>=0; i--)
-        {
-            postfix[i]=nums[i]*postfix[i+1];
+            postfix*=nums[i];
         }
+        ans.push_back(postfix);
 
-        answer[0]=postfix[1];
-        answer[nums.size()-1]=prefix[nums.size()-2];
-        for(int i=1; i<nums.size()-1; i++)
+        for(int i=1; i<nums.size(); i++)
         {
-            answer[i]=prefix[i-1]*postfix[i+1];
-        }
+            if(nums[i]==0)
+            {
+                postfix=1;
+                for(int j=i+1; j<nums.size(); j++)
+                {
+                    postfix*=nums[j];
+                }
+            }
+            else
+                postfix=postfix/nums[i];
 
-        return answer;
+            prefix*=nums[i-1];
+
+            ans.push_back(prefix*postfix);
+        }
+        return ans;
     }
 };
