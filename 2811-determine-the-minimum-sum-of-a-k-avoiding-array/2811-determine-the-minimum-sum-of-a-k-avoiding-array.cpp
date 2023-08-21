8a6fe3e4a340abbe9ec8b93ba73ed8d234d3b422
PiyushMohata <98268606+PiyushMohata@users.noreply.github.com>
2023-08-22 01:18:41 +0530

    Time: 23 ms (30.76%), Space: 7.5 MB (80.67%) - LeetHub

---
+++ 2811-determine-the-minimum-sum-of-a-k-avoiding-array/2811-determine-the-minimum-sum-of-a-k-avoiding-array.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int minimumSum(int n, int k) {
+        int sum=0,count=0,i=2;
+        vector<int>v;
+        v.push_back(1);
+
+         if (k == 1) {
+            return n*(n+1)/2;
+        }
+        
+        while(v.size()!=n)
+        {
+            for(int j=1; j<i; j++)
+            {
+                if(i+j==k)
+                {
+                    break;
+                }
+                else
+                {
+                    count++;
+                }
+            }
+            if(count==i-1)
+            {
+                v.push_back(i);
+                count=0;
+            }
+            else
+                count=0;
+            i++;
+        }
+
+        for(auto x:v)
+        {
+            sum+=x;
+        }
+        for(int i=0; i<v.size(); i++)
+        {
+            cout<<v[i]<<" ";
+        }
+        return sum;
+    }
+};
