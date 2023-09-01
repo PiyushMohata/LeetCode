4227632792b76fde53fbf43e4b60baf492f16c2a
PiyushMohata <98268606+PiyushMohata@users.noreply.github.com>
2023-09-01 23:57:35 +0530

    Time: 0 ms (100%), Space: 6 MB (27.16%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+       int result=0;
+       int x=32;
+       while(x--)
+       {
+           result=result<<1;
+           if(n&1==1)
+           {
+               result++;
+           }
+           n=n>>1;
+       }
+       return result;
+    }
+};
