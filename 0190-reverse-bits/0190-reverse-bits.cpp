196203e785b932f26b3456fc0b26c7af9a24051e
PiyushMohata <98268606+PiyushMohata@users.noreply.github.com>
2023-09-02 00:10:38 +0530

    Time: 2 ms (49.62%), Space: 6 MB (27.16%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -3,15 +3,9 @@ public:
     uint32_t reverseBits(uint32_t n) {
        int result=0;
        int x=32;
-       if(n==0)
-        return 0;
+
        while(x--)
        {
-           if(n==0)
-           {
-               result=result<<x+1;
-               return result;
-           }
            result=result<<1;
            if(n&1==1)
            {
