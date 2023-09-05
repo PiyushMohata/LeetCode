227bee3157800761006b975712e1abdc0e77fda1
PiyushMohata <98268606+PiyushMohata@users.noreply.github.com>
2023-09-05 19:19:49 +0530

    Time: 6 ms (39.83%), Space: 8.4 MB (55.82%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.cpp
+++ 0682-baseball-game/0682-baseball-game.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int calPoints(vector<string>& op) {
-        stack<int>s;
+        stack<int>s;//create a stack
         int sum1=0;
 
         for(int i=0; i<op.size(); i++)
@@ -9,27 +9,31 @@ public:
             int temp=0,sum=0;
             if(op[i]=="+" && !s.empty())
             {
+                // we need to make a temporary variable in order to access the value before top
+                // store the value of top in temp and then pop the top value so that we can get the new top value and perform sum
+                  
                 temp=s.top();
                 s.pop();
                 sum = temp + s.top();
-                s.push(temp);
-                s.push(sum);
+                s.push(temp);// again push the temp variable in the stack
+                s.push(sum);// push the sum we got
             }
-            else if(op[i]=="C" && !s.empty())
+            else if(op[i]=="C" && !s.empty())// it is important to check if the stack value is not empty
             {
+                // if the value is C then we simply need to pop from stack
                 s.pop();
             }
             else if(op[i]=="D" && !s.empty())
             {
-                s.push(2*s.top());
+                s.push(2*s.top());// push the value 2 times the top value
             }
             else
             {
-                s.push(stoi(op[i]));
+                s.push(stoi(op[i]));// convert string value into integer using stoi function and push into stack
             }
         }
 
-        while (!s.empty()) 
+        while (!s.empty()) // iterate through stack till its empty and do the sum
         {
             sum1 += s.top();
             s.pop();
