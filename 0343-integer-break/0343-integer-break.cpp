9de2ef30205bc11cfd8216c5d15901f730a24ccd
PiyushMohata <98268606+PiyushMohata@users.noreply.github.com>
2023-10-06 21:02:29 +0530

    Time: 0 ms (100%), Space: 6.5 MB (29.03%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,40 +1,11 @@
 class Solution {
 public:
     int integerBreak(int n) {
-        int x=1;
-        vector<int>v;
-        if(n==2)
-            return 1;
-        else if(n==3)
-            return 2;
-        while(1)
-        {
-            if(n-3==0)
-            {
-                n=n-3;
-                v.push_back(3);
-                break;
-            }
-            else if(n-2==0)
-            {
-                n=n-2;
-                v.push_back(2);
-                break;
-            }
-            else if(n-1==0)
-            {
-                v[v.size()-1]+=1;
-                break;
-            }
-            n=n-3;
-            v.push_back(3);
-        }
-   
-        for(int i=0; i<v.size(); i++)
-        {
-            x*=v[i];
-        }
-
-        return x;
+        if(n<=3)
+            return n-1;
+        int a=n/3,b=n%3;
+        if(b==0)return pow(3,a);
+        else if(b==1)return pow(3,a-1)*4;
+        return pow(3,a)*2;
     }
 };
